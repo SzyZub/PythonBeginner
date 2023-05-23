5ed34014cf9e29cd9d60df1207625349df350b30
Szymon Żuber <tornaos21@gmail.com>
2023-05-23 10:19:52 +0200

    Finished all

--- Lesson7/Openingfile.py
+++ Lesson7/Openingfile.py
@@ -1,5 +1,6 @@
 # Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
+import os
+fname = "Lesson7/" + input("Enter file name: ") 
 fh = open(fname)
 count = 0
 sa = 0
